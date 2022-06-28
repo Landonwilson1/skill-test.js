4176dcae26a7a6ce372d31b264c6fb197b7ff09c
Landon Wilson <77355267+Landonwilson1@users.noreply.github.com>
2022-06-28 16:58:55 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import CausesGrid from './components/causes/CausesGrid'
 import Search from './components/ui/Search'
 import './App.css'
 
+//State for data, query, and, loading
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [query, setQuery] = useState('')
 
+//fetch causes data from api with useEffect and axios
   useEffect(() => {
     const fetchItems = async () => {
       setIsLoading(true)
+      //Was not able to make query parameters work in time
       const result = await axios(
         `https://api.givebacks.com/services/core/causes/search?name=${query}`
       )
 
       console.log(result.data)
-
+      
+      //set data to items state
       setItems(result.data)
       setIsLoading(false)
     }
@@ -30,6 +34,7 @@ const App = () => {
     setQuery(q)
   }
   
+  //components 
   return (
     <div className='container'>
       <Header />
